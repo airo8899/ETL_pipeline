7e244416f0566fec791d258e112a47ecacb18002
Alexey Poptsov <alexey.poptsov@mail.ru>
2022-04-24 18:42:21 +0000

    add select

--- dags/a.poptsov/airflow.py
+++ dags/a.poptsov/airflow.py
@@ -1,1 +1,10 @@
 import pandas as pd
+
+from airflow import DAG
+from airflow.operators.python import PythonOperator # Так как мы пишет такси в питоне
+from datetime import datetime 
+
+query = 
+"""
+select *
+"""
